397b5300a3012db84a12a2af23ad2a7d5d5b0552
Skylar Shyu <ps.shyu@gmail.com>
2018-02-17 18:14:37 -0700

    Q1 setup

---
+++ Assignment2/Q1.m
@@ -0,0 +1,8 @@
+
+n = [17 33 65 129 257 513]; % N-values
+
+% Constant Interpolant (composite midpoint rule)
+
+% Linear Interpolant (composite trapezoid rule)
+
+% Quadratic Interpolant (composite Simpson formula)
