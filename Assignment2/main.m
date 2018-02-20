4fb6590d6895d60459c9bd37d9450f7a3a6b56da
Skylar Shyu <ps.shyu@gmail.com>
2018-02-19 18:59:17 -0700

    Implemented midpoint

--- Assignment2/main.m
+++ Assignment2/main.m
@@ -2,5 +2,6 @@
 a = 1;
 b = 2;
 N = 28;
-[xi, wi] = QuadSchemeTrapezoid(a,b,N);
+[xi, wi] = QuadSchemeMidpoint(a,b,N);
+%[xi, wi] = QuadSchemeTrapezoid(a,b,N);
 %[xi, wi] = QuadSchemeSimpson(a,b,N);
