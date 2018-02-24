fdd05305e3f6b6b0210b31c73b5791529006f63f
Skylar Shyu <ps.shyu@gmail.com>
2018-02-23 19:41:24 -0700

    Gauss Plots added

--- Assignment2/saveFigures.m
+++
@@ -1,7 +0,0 @@
-% the code below was used to save figures into the output folder as jpg
-% images in the specified directory. Parts of it are hard-coded, so it wont
-% work on all machines
-function saveFigures(fig,assignmentNum,fileTitle, type)
-folder = strcat('C:\Users\Skylar\Documents\GitHub\CS3200\Assigment',num2str(assignmentNum),'\output');
-fileName = sprintf(fileTitle);
-saveas(fig, fullfile(folder, fileName), type);
