307a0823b8bd7ed2befbfd47a63491889b976037
Skylar Shyu <skylar.shyu@utah.edu>
2018-04-04 17:23:38 -0600

    done q1. need to split q2 and do q3.

--- Assignment4/functions/q2.m
+++ Assignment4/functions/q2.m
@@ -1,35 +1,37 @@
 n = 100;  % set some number of iterations , may need adjusting
-n_space = linspace(1,100,100);
+error = 10^-15;
 
 % initial values for iteration
 x_values = zeros(2,n);
-x_values(1,1) = 1.0;
-x_values(2,1) = 0.1;
+x_values(1,1) = 2;
 
 % pair of equations to solve
 % from here on out, x = x1 and y = x2 in the assignment
 syms x y;               % create the symbols for functions
-f1 = @(x,y) (x^3+y-1);  % f1
-f2 = @(x,y) (y^3-x-1);  % f2
+f = @(x) (exp(x-1)+x^2-4);  % f
 
 % Calculate the entries for the Jacobian matrix
-a = matlabFunction(diff(f1(x,y),x)); % df1_dx
-b = matlabFunction(diff(f2(x,y),x)); % df2_dx = -1
-c = matlabFunction(diff(f1(x,y),y)); % df1_dy = 1
-d = matlabFunction(diff(f2(x,y),y)); % df2_dy
+a = matlabFunction(diff(f(x),x)); % df1_dx
+%b = matlabFunction(diff(f2(x,y),x)); % df2_dx = -1
+%c = matlabFunction(diff(f1(x,y),y)); % df1_dy = 1
+%d = matlabFunction(diff(f2(x,y),y)); % df2_dy
 
-det = matlabFunction(a(x)*d(y)-b()*c());
-dx = matlabFunction(-(d(y)*f1(x,y)-c()*f2(x,y))/det(x,y));
-dy = matlabFunction(-(-1*b()*f1(x,y)+a(x)*f2(x,y))/det(x,y));
 
 for i = 1:n-1
-    x = x_values(:,i); % x values for this iteration
-    J_x = [a(x(1)) b(); c() d(x(2))]; %calculate the Jacobian matrix
-    x_next = x - inv(J_x) * [f1(x(1),x(2)); f2(x(1),x(2))];
-    x_values(:,i+1) = x_next;
+    x = x_values(1,i); % x values for this iteration
+    ay = a(x);
+    delta_x = -inv(a(x))*f(x); 
+    x_next = x + delta_x;
+    x_values(1,i+1) = x_next;
+    if f(x_next(1,1)) < error
+        n = i;
+        break;
+    end
 end
+
+n_space = linspace(1,n,n);
 figure('Name', 'x-values for each Iteration')         % figure title              
-plot(n_space, x_values(1,:), 'o-', n_space, x_values(2,:), '.-.');                     % plotting Composite Midpoint error against error = 0
+plot(n_space, a(x_values(1,1:n)), 'o-');                   % plotting Composite Midpoint error against error = 0
 legend show;                                        % show legends!
 %legend('Composite Midpoint Rule Error');            % set legends
 grid on;                                            % turn grids on
---
+++ Assignment4/functions/scratchpad.m
@@ -0,0 +1,3 @@
+fun = @root2d;
+x0 = [x_values(1,1),x_values(2,1)];
+exact = fsolve(fun,x0);
